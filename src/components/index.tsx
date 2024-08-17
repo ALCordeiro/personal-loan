b88720e62e7dd65122ce53f66d518f11e786d228
Alysson Cordeiro <alysson.cordeiro@ab-inbev.com>
2024-08-17 14:28:10 -0300

    IMports fix

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -4,3 +4,4 @@ export { default as Terms } from './Terms';
 export { default as UpsellOpportunity } from './UpsellOpportunity';
 export { default as Error } from './Error';
 export { default as CardLoan } from './CardLoan';
+export { default as Button } from './Button';
